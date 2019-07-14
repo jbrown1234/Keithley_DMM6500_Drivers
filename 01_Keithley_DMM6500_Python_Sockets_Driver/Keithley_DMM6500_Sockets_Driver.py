41cb1f55157e2acd13c19d34d206f8fad185914a
Josh Brown <39962766+jbrown1234@users.noreply.github.com>
2019-07-14 08:25:45 -0400

    Update Keithley_DMM6500_Sockets_Driver.py

--- 01_Keithley_DMM6500_Python_Sockets_Driver/Keithley_DMM6500_Sockets_Driver.py
+++ 01_Keithley_DMM6500_Python_Sockets_Driver/Keithley_DMM6500_Sockets_Driver.py
@@ -144,6 +144,76 @@ class DMM6500:
     def Measure(self, count):
         sndBuffer = "print(dmm.measure.read())"
         return self.QueryCmd(sndBuffer, 32)
+
+    def SetFunction_Temperature(self, *args):
+        # This function can be used to set up to three different measurement
+        # function attributes, but they are expected to be in a certain
+        # order....
+        #   For simple front/rear terminal measurements:
+        #       1. Transducer (TC/RTD/Thermistor)
+        #       2. Transducer type
+        #   For channel scan measurements:
+        #       1. Channel string
+        #       2. Transducer
+        #       3. Transducer type
+        if (type(args[0]) != str):
+            print("dmm.measure.func = dmm.FUNC_TEMPERATURE")
+        else:
+            setStr = "channel.setdmm(\"{}\", ".format(args[0])
+            print("{}dmm.ATTR_MEAS_FUNCTION, dmm.FUNC_TEMPERATURE)".format(setStr))
+            if(len(args) > 1):
+                if(args[1] == self.Transducer.TC):
+                   xStr = "dmm.TRANS_THERMOCOUPLE"
+                   xStr2 = "dmm.ATTR_MEAS_THERMOCOUPLE"
+                elif(args[1] == self.Transducer.RTD4):
+                   xStr = "dmm.TRANS_FOUR_RTD"
+                   xStr2 = "dmm.ATTR_MEAS_FOUR_RTD"
+                elif(args[1] == self.Transducer.RTD3):
+                   xStr = "dmm.TRANS_THREE_RTD"
+                   xStr2 = "dmm.ATTR_MEAS_THREE_RTD"
+                elif(args[1] == self.Transducer.THERM):
+                   xStr = "dmm.TRANS_THERMISTOR"
+                   xStr2 = "dmm.ATTR_MEAS_THERMISTOR"
+                sndBuffer = "{}dmm.ATTR_MEAS_TRANSDUCER, {})".format(setStr, xStr)
+                self.SendCmd(sndBuffer)
+            if(len(args) > 2):
+                if(args[1] == self.Transducer.TC):
+                    if(args[2] == self.TCType.K):
+                       xType = "dmm.THERMOCOUPLE_K"
+                    elif(args[2] == self.TCType.J):
+                       xType = "dmm.THERMOCOUPLE_J"
+                    elif(args[2] == self.TCType.N):
+                       xType = "dmm.THERMOCOUPLE_N" 
+                    #print("{}dmm.ATTR_MEAS_THERMOCOUPLE, {})".format(setStr, xType))
+                    sndBuffer = "{}dmm.ATTR_MEAS_THERMOCOUPLE, {})".format(setStr, xType)
+                    self.SendCmd(sndBuffer)
+                elif((args[1] == self.Transducer.RTD4) or (args[1] == self.Transducer.RTD3)):
+                    if(args[2] == self.RTDType.PT100):
+                       rtdType = "dmm.RTD_PT100"
+                    elif(args[2] == self.RTDType.PT385):
+                       rtdType = "dmm.RTD_PT385"
+                    elif(args[2] == self.RTDType.PT3916):
+                       rtdType = "dmm.RTD_PT3916"
+                    elif(args[2] == self.RTDType.D100):
+                       rtdType = "dmm.RTD_F100"
+                    elif(args[2] == self.RTDType.F100):
+                       rtdType = "dmm.RTD_D100"
+                    elif(args[2] == self.RTDType.USER):
+                       rtdType = "dmm.RTD_USER"
+                    #print("{}{}, {})".format(setStr, xStr2, rtdType))
+                    sndBuffer = "{}{}, {})".format(setStr, xStr2, rtdType)
+                    self.SendCmd(sndBuffer)
+                if(args[1] == self.Transducer.THERM):
+                    if(args[2] == self.ThermType.TH2252):
+                       thrmType = "dmm.THERM_2252"
+                    elif(args[2] == self.ThermType.PT385):
+                       thrmType = "dmm.THERM_5000"
+                    elif(args[2] == self.ThermType.PT3916):
+                       thrmType = "dmm.THERM_10000"
+                    #print("{}{}, {})".format(setStr, xStr2, thrmType))
+                    sndBuffer = "{}{}, {})".format(setStr, xStr2, thrmType)
+                    self.SendCmd(sndBuffer)
+        return
     
     class MeasFunc(Enum):
         DCV = 0
@@ -160,4 +230,27 @@ class DMM6500:
     class FilterType(Enum):
         REP = 0
         MOV = 1
-    
+
+    class Transducer(Enum):
+        TC = 0
+        RTD4 = 1
+        RTD3 = 2
+        THERM = 3
+
+    class TCType(Enum):
+        K = 0
+        J = 1
+        N = 2
+        
+    class RTDType(Enum):
+        PT100 = 0
+        PT385 = 1
+        PT3916 = 2
+        D100 = 3
+        F100 = 4
+        USER = 5
+
+    class ThermType(Enum):
+        TH2252 = 0
+        TH5K = 1
+        TH10K = 2
